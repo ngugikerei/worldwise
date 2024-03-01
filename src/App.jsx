b8fb888065d6b59375ad1d4da895929e3580a825
Rununu Games <ngugikerei@gmail.com>
2024-03-01 09:07:43 +0300

    setroutes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Product from './Pages/Product';
+import Pricing from './Pages/Pricing';
+import Homepage from './Pages/Homepage';
+import PageNotFound from './Pages/PegeNotFound';
+
 export default function Worldwise() {
-  return <div> Worldwise</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ src/Pages/PegeNotFound.jsx
@@ -0,0 +1,3 @@
+export default function PegeNotFound() {
+  return <div>PegeNotFound</div>;
+}
