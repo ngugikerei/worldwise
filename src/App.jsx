3432588f20f9c1c84aeb41173e5a9dae35a6d89b
Rununu Games <ngugikerei@gmail.com>
2024-03-04 09:48:10 +0300

    chnage modules and styles

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,18 @@ import Pricing from './Pages/Pricing';
 import Homepage from './Pages/Homepage';
 import PageNotFound from './Pages/PegeNotFound';
 import AppLayout from './Pages/AppLayout';
+import Login from './Pages/Login';
 
 export default function Worldwise() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
-        <Route path="*" element={<PageNotFound />} />
+        <Route path="product" element={<Product />} />
+        <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,20 +1,20 @@
-import styles from "./City.module.css";
+import styles from './City.module.css';
 
 const formatDate = (date) =>
-  new Intl.DateTimeFormat("en", {
-    day: "numeric",
-    month: "long",
-    year: "numeric",
-    weekday: "long",
+  new Intl.DateTimeFormat('en', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+    weekday: 'long',
   }).format(new Date(date));
 
 function City() {
   // TEMP DATA
   const currentCity = {
-    cityName: "Lisbon",
-    emoji: "🇵🇹",
-    date: "2027-10-31T15:59:59.138Z",
-    notes: "My favorite city so far!",
+    cityName: 'Lisbon',
+    emoji: '🇵🇹',
+    date: '2027-10-31T15:59:59.138Z',
+    notes: 'My favorite city so far!',
   };
 
   const { cityName, emoji, date, notes } = currentCity;
@@ -51,9 +51,7 @@ function City() {
         </a>
       </div>
 
-      <div>
-        <ButtonBack />
-      </div>
+      <div>{/* <ButtonBack /> */}</div>
     </div>
   );
 }
