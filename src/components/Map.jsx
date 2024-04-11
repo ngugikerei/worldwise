6a9878cb2e56c81aa9a9e7c47e1123cef5b9a7cb
Rununu Games <ngugikerei@gmail.com>
2024-04-11 08:54:54 +0300

    fix useMap

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,29 +1,34 @@
 import styles from './Map.module.css';
-
+//React hooks
 import { useEffect, useState } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
-import { useCities } from '../../contexts/CitiesContext';
 
-import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet';
-import Button from './Button';
+//Custom Hooks
+import { useCities } from '../../contexts/CitiesContext';
 import { useGeoLocation } from '../../hooks/useGeolocation';
 import { useUrlPosition } from '../../hooks/useUrlPostion';
 
+//React Components
+import {
+  MapContainer,
+  TileLayer,
+  useMap,
+  Marker,
+  Popup,
+  useMapEvents,
+} from 'react-leaflet';
+import Button from './Button';
+
 export default function Map() {
   //useNavigate hook for navigating to form element in map
-  const navigate = useNavigate();
+
   const { cities } = useCities();
 
   const { isPostionLoading, error, geolocationPosition, getPosition } =
     useGeoLocation();
 
   const [mapPosition, setMapPosition] = useState([40, 0]);
-
-  // const [searchParams, setSearchParams] = useSearchParams();
-  // const mapLat = searchParams.get('lat');
-  // const mapLng = searchParams.get('lng');
-
-  const { lat, lng } = useUrlPosition();
+  const [lat, lng] = useUrlPosition();
 
   useEffect(
     function () {
@@ -32,6 +37,7 @@ export default function Map() {
     [lat, lng]
   );
 
+  //Sync your mapPosition state variable to lat,lng variables
   useEffect(
     function () {
       if (geolocationPosition)
@@ -47,19 +53,17 @@ export default function Map() {
           {isPostionLoading ? 'Loading...' : '  Use my location'}
         </Button>
       )}
-      {/* use React Leaflet Library to Display Posion on Mao, based on passed co-ordinates */}
+      {/* use React Leaflet Library to  Display Posion on Map, based on passed co-ordinates */}
       <MapContainer
         className={styles.map}
         center={mapPosition}
-        zoom={5}
+        zoom={6}
         scrollWheelZoom={true}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
-        {}
-
         {cities.map((city) => (
           <Marker
             position={[city.position.lat, city.position.lng]}
@@ -71,7 +75,31 @@ export default function Map() {
             </Popup>
           </Marker>
         ))}
+
+        <ChangeCenter position={mapPosition} />
+        <DetectClick />
       </MapContainer>
     </div>
   );
+}
+
+//move map to currently seleected city
+function ChangeCenter({ position }) {
+  //useMap hook gets current instance of map being displayed, from Leaflet
+  const map = useMap();
+  map.setView(position);
+  return null;
+}
+
+//Open form when map is clicked
+function DetectClick() {
+  const navigate = useNavigate();
+
+  // useMapEvents defines different properties fro different map events, takes callback
+  useMapEvents({
+    click: (e) => {
+      //query for getting lat and lng from clicked postion on map
+      navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`);
+    },
+  });
 }
